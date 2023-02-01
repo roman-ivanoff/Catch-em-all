35fd941056069cd5412aec9da3e43b26d985913d
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-02-01 22:37:14 +0200

    setup

---
+++ Catch’em all/Supporting Files/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Catch’em all
+//
+//  Created by Roman Ivanov on 20.01.2023.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = ViewController()
+        let navController = UINavigationController(rootViewController: rootViewController)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+
+}
+
