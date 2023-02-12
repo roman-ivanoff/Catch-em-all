f55b599344240f59c73a10ec322c13ef2b373cd0
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-02-12 22:22:16 +0200

    added model

---
+++ Catch_em_all/Models/Species.swift
@@ -0,0 +1,13 @@
+//
+//  Species.swift
+//  Catch_em_all
+//
+//  Created by Roman Ivanov on 12.02.2023.
+//
+
+import Foundation
+
+struct Species: Decodable {
+    let name: String
+    let url: String
+}
