65e08f58e0b0108c0dbdbaa78013496d712391d0
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-02-13 18:48:37 +0200

    added model

---
+++ Catch_em_all/Models/OfficialArtwork.swift
@@ -0,0 +1,12 @@
+//
+//  OfficialArtwork.swift
+//  Catch_em_all
+//
+//  Created by Roman Ivanov on 13.02.2023.
+//
+
+import Foundation
+
+struct OfficialArtwork: Decodable {
+    let frontDefault: String
+}
