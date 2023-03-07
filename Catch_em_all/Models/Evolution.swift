fb12a7ff0fee0c900cd9419d71c4ecc8aac4ed66
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-03-07 22:12:15 +0200

    added model

---
+++ Catch_em_all/Models/Evolution.swift
@@ -0,0 +1,13 @@
+//
+//  Evolution.swift
+//  Catch_em_all
+//
+//  Created by Roman Ivanov on 07.03.2023.
+//
+
+import Foundation
+
+struct Evolution: Decodable {
+    let chain: Chain
+    let id: Int
+}
