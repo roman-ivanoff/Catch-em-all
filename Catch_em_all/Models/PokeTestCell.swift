1ebda2cdb940fc6a59711b4164c6e645ab3d086c
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-03-07 22:13:08 +0200

    added model

---
+++ Catch_em_all/Models/PokeTestCell.swift
@@ -0,0 +1,14 @@
+//
+//  PokeTestCell.swift
+//  Catch_em_all
+//
+//  Created by Roman Ivanov on 07.03.2023.
+//
+
+import Foundation
+
+struct PokeTestCell {
+    let pokeName: String
+//    let pokeImage: UIImage
+    let pokeSkill: String
+}
