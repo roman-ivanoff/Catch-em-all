19c483a2ad7f4c3a619fd1c6259cd16002148e63
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-04-26 17:34:45 +0300

    added core data methods to AppDelegate

--- Catch_em_all/Supporting Files/AppDelegate.swift
+++ Catch_em_all/Supporting Files/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Hero
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -25,6 +26,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = window
 
         return true
+    }
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "check")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 }
