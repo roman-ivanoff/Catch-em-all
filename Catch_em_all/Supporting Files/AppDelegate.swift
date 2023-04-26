44cdfd1a5648d08af298f8ec522e02939cf84b7c
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-04-26 17:38:44 +0300

    added core data

--- Catch_em_all/Supporting Files/AppDelegate.swift
+++ Catch_em_all/Supporting Files/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "check")
+        let container = NSPersistentContainer(name: "Pokemon")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
